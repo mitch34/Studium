c661a1c5e884f19096eeeb482f05c052de7d8f85
Michel Stephan <Michel.Stephan85@gmx.de>
2017-11-01 14:03:41 +0100

    Neu

--- Programmieren_1/Uebungen/Primzahl.java
+++ Programmieren_1/Uebungen/Primzahl.java
@@ -2,37 +2,48 @@ import java.util.Scanner;
 
 public class Primzahl
 {
-	public static void main (String [] args)
-	{
+   public static void main (String [] args)
+   {
+      // Variablen	
+	  int zahl = 0;
 		
-	   int zahl = 0;
-		
-	   System.out.println("Bitte geben Sie ein Zahl ein:");
-	   Scanner in = new Scanner(System.in);
-	   zahl = in.nextInt();
-		
-	   if ( zahl < 8)
+	  System.out.println("Bitte geben Sie ein Zahl ein:");
+	  
+	  // Eingabe von Zahlen
+	  Scanner in = new Scanner(System.in);
+	  zahl = in.nextInt();
+	  
+      /*
+	   * Überprüfen der Variable: zahl,
+	   * zahl < 8, Ausgabe: "Spinner". Abbruch der If-Verzweigung
+	   * zahl > 100, Ausgabe: "Zu schwer". Abbruch der If-Verzweigung
+	   *
+	   * sonst, überprüfen der Variable: zahl 
+	   * Ist zahl durch 2, 3, 5, 7 teilbar ohne Rest, dann Ausgabe: "Niete"
+	   * andersfall ist die Variable: zahl , eine Primzahl. Ausgabe: "Primzahl"
+	   */
+	  if ( zahl < 8)
+	  {
+	     System.out.println("Spinner");
+	  }
+	  else
+	  {
+		if ( zahl > 100)
 		{
-			System.out.println("Spinner");
+			System.out.println("Zu schwer");
 		}
 		else
 		{
-			if ( zahl > 100)
+			if ( zahl % 2 == 0 || zahl % 3 == 0 || zahl % 5 == 0 || zahl % 7 == 0)
 			{
-				System.out.println("Zu schwer");
+				System.out.println("Niete");
 			}
 			else
 			{
-				if ( zahl % 2 == 0 || zahl % 3 == 0 || zahl % 5 == 0 || zahl % 7 == 0)
-				{
-					System.out.println("Niete");
-				}
-				else
-				{
-					System.out.println("Primzahl");
-				}
+				System.out.println("Primzahl");
 			}
 		}
+	  }
 		
 	}
 }
--- Programmieren_1/Uebungen/Siebzehn.java
+++ Programmieren_1/Uebungen/Siebzehn.java
@@ -1,26 +1,27 @@
 import java.util.Scanner;
 
-public class Siebzehn{
-	public static void main(String [] args)
-	{
-	   // Variablen	
-	   int zahl;
-	   int Restwert;
+public class Siebzehn
+{
+   public static void main(String [] args)
+   {
+      // Variablen	
+	  int zahl;
+	  int Restwert;
 	   
-	   System.out.println("Bitte geben Sie eine Zahl ein die durch 17 dividiert wird:");
+	  System.out.println("Bitte geben Sie eine Zahl ein die durch 17 dividiert wird:");
 	   
-	   // Eingabe von Zahlen 
-	   Scanner in = new Scanner(System.in);
-	   zahl = in.nextInt();
+	  // Eingabe von Zahlen 
+	  Scanner in = new Scanner(System.in);
+	  zahl = in.nextInt();
 	
-	   // Restwertberechnung
-	   Restwert = zahl % 17; 
+	  // Restwertberechnung
+	  Restwert = zahl % 17; 
 	
-	   // Ausgabe vom Restwert
-	   System.out.println("Bei der Division von 17 betraegt der Rest: "+Restwert);
+	  // Ausgabe vom Restwert
+	  System.out.println("Bei der Division von 17 betraegt der Rest: "+Restwert);
 	
-	   // Restwert = 13, Ausgabe: "Pechkeks." sonst Keine Ausgabe
-	   if (zahl % 17 == 13)
-	      System.out.println("Pechkeks.");
+	  // Restwert = 13, Ausgabe: "Pechkeks." sonst Keine Ausgabe
+	  if (zahl % 17 == 13)
+	     System.out.println("Pechkeks.");
 	}
 }
