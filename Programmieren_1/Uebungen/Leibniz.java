0912189f6da496890f3cd1a379be8b8cfe48d391
Michel Stephan <Michel.Stephan85@gmx.de>
2017-11-10 00:11:29 +0100

    neu programm leibniz

--- Programmieren_1/Uebungen/Leibniz.java
+++ Programmieren_1/Uebungen/Leibniz.java
@@ -6,36 +6,28 @@ public class Leibniz {
     // Variablen
 	int n = 0;
 	double pi = 0;
+	double div = 1;
 	
 	Scanner in = new Scanner(System.in);
 	n = in.nextInt();
 	
-	/*
+	
 	for (int x = 0; x < n; x++) {
-		int div = 2 * x +1;
+		
+		//System.out.println("divrunde:" + x + "Ausgabe:" +div);
 		if ( x % 2 == 0) 
 		{
 			pi = pi + (1 / div);
+			//System.out.println("divrundeGerade:" + x + "Ausgabe:" +pi);
 		}
 		else
 		{
 			pi = pi - (1 / div);
+			//System.out.println("divrundeUnGerade:" + x + "Ausgabe:" +pi);
 		}
+		div = div + 2;
 	}
-	// pi = pi * 4;
+	 pi = pi * 4;
 	System.out.println(pi);
-	*/
-	double denominator = 1;
-	for (int x = 0; x < n; x++) {
- 
-         if (x % 2 == 0) {
-            pi = pi + (1 / denominator);
-         } else {
-            pi = pi - (1 / denominator);
-         }
-         denominator = denominator + 2;
-      }
-     // pi = pi * 4;
-      System.out.println(pi);
    }
 }
