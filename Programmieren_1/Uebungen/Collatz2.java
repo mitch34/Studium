322bcf1fa601b178a5ee5ff91b23e93ad733b8e9
Michel Stephan <michel.stephan85@gmx.de>
2017-11-09 13:31:50 +0100

    neu programmieren

--- Programmieren_1/Uebungen/Collatz2.java
+++ Programmieren_1/Uebungen/Collatz2.java
@@ -4,8 +4,32 @@ public class Collatz2 {
 	public static void main ( String [] args ) {
 		
 		// Variablen
-		
 		int n = 0;
+		int laenge = 1;
+		int start = 0;
+		int i = 1;
+		Scanner in = new Scanner(System.in);
+		n = in.nextInt();
 		
+		while (i <= n) {
+			start = i;
+			laenge = 0;
+			while (start > 1) {
+				if (start%2 == 0)
+				{
+					start = start / 2;
+				}
+				else
+				{
+					start = 3 * start + 1;
+				}
+				System.out.print(start + " ");
+				laenge++;
+			}
+		System.out.println("\nLaenge ist " + laenge);
+		if (laenge == n)
+			break;
+		i++;
+		}
 	}
 }
--- Programmieren_1/Uebungen/Leibniz.java
+++ Programmieren_1/Uebungen/Leibniz.java
@@ -3,20 +3,39 @@ import java.util.Scanner;
 public class Leibniz {
 	public static void main(String[] args) {
  
-      int count = 999999999;
-      double pi = 0;
-      double denominator = 1;
+    // Variablen
+	int n = 0;
+	double pi = 0;
+	
+	Scanner in = new Scanner(System.in);
+	n = in.nextInt();
+	
+	/*
+	for (int x = 0; x < n; x++) {
+		int div = 2 * x +1;
+		if ( x % 2 == 0) 
+		{
+			pi = pi + (1 / div);
+		}
+		else
+		{
+			pi = pi - (1 / div);
+		}
+	}
+	// pi = pi * 4;
+	System.out.println(pi);
+	*/
+	double denominator = 1;
+	for (int x = 0; x < n; x++) {
  
-      for (int x = 0; x < count; x++) {
- 
-         if ((x&1) == 0) {
+         if (x % 2 == 0) {
             pi = pi + (1 / denominator);
          } else {
             pi = pi - (1 / denominator);
          }
          denominator = denominator + 2;
       }
-      pi = pi * 4;
+     // pi = pi * 4;
       System.out.println(pi);
    }
 }
